cd5672b3187000ad89330ec03d1e39c3fa5d998b
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-01-28 15:44:25 -0300

    login com conta google

--- src/app/servicosInterface/autenticacao-firebase.service.ts
+++ src/app/servicosInterface/autenticacao-firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { Auth} from '@angular/fire/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, updateProfile,getAuth } from 'firebase/auth';
 import { authState } from 'rxfire/auth';
 import { from, switchMap } from 'rxjs';
 
@@ -10,6 +10,7 @@ import { from, switchMap } from 'rxjs';
 export class AutenticacaoFirebaseService {
 
   usuarioLogado$ = authState(this.usuarioFb);
+  auth = getAuth()
 
   constructor(
     private usuarioFb: Auth
@@ -27,5 +28,27 @@ export class AutenticacaoFirebaseService {
       return from(createUserWithEmailAndPassword(this.usuarioFb, email, senha)).pipe(
         switchMap(({user}) => updateProfile(user, {displayName: nome}))
       )
+    }
+
+    logarGoogle(){
+      const provider = new GoogleAuthProvider();
+      return from(signInWithPopup(this.auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential!.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.email;
+          // The AuthCredential type that was used.
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        }))
     }
 }
