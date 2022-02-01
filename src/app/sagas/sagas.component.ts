f1218bb0fa81df11354b70fac9a5d1dbff7cd0ec
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-02-01 16:58:32 -0300

    criação do campo de busca em sagas

--- src/app/sagas/sagas.component.ts
+++ src/app/sagas/sagas.component.ts
@@ -1,8 +1,8 @@
 import { MatDialog } from '@angular/material/dialog';
 import { Sagas } from './../modelosInterface/sagas';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { catchError, fromEvent, Observable,filter, debounceTime, distinctUntilChanged, tap, of } from 'rxjs';
 import { SagasService } from '../servicosInterface/sagas.service';
 import { map } from 'rxjs/operators';
 import { SagasDialogoComponent } from '../sagas-dialogo/sagas-dialogo.component';
@@ -13,9 +13,12 @@ import { ConnectedOverlayPositionChange } from '@angular/cdk/overlay';
   templateUrl: './sagas.component.html',
   styleUrls: ['./sagas.component.scss']
 })
-export class SagasComponent {
+export class SagasComponent implements AfterViewInit {
 
   sagas$: Observable<Sagas[]>;
+  result$?: Observable<Sagas[]>
+  value!: string;
+
   sagas= this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
@@ -36,6 +39,26 @@ export class SagasComponent {
           return of([])
         })
       )
+  }
+
+  @ViewChild('searchInput') searchInput!: ElementRef
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+      filter(Boolean),
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(() => {
+        const query = this.searchInput.nativeElement.value
+        // console.log(query)
+        if(query) {
+          this.result$ = this.sagasService.pesquisar(query)
+          console.log(this.result$)
+        } else {
+          this.result$ = undefined
+        }
+      })
+    ).subscribe()
   }
 
   abrirSagas(livros: [{nome:string}]){
